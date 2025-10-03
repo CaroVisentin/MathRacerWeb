8e8f817e6e008d5aab46f4b7649787dcffa504e1
Bianca Leites <bleites@circostudio.com>
2025-10-03 18:20:45 -0300

    fix home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,9 @@
-import { Comodines } from "../../shared/comodines/comodines"
 
 export const Home = () => {
 
     return (
         <>
             <p> Home </p>
-            <Comodines matafuego={3} sync={5} thunder={5}/>
         </>
     )
 }
