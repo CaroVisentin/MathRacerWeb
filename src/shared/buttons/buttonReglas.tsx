256860f639cbd7771a6114cebb4861c7d11ed383
Bianca Leites <bleites@circostudio.com>
2025-10-09 22:32:04 -0300

    avances modo historia

--- src/shared/buttons/buttonReglas.tsx
+++ src/shared/buttons/buttonReglas.tsx
@@ -1,17 +1,21 @@
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 
 interface ButtonReglasProps {
-    onClick: () => void;
+    onClick?: () => void;
 }
 
 export const ButtonReglas = ({ onClick }: ButtonReglasProps) => {
+    const navigate = useNavigate();
+    const handleClick = onClick ?? (() => navigate("/reglas"));
+
     return (
         <>
             <div className="flex justify-end w-full px-6 py-4 mb-8">
                 <button
                     className="border border-cyan-400 p-2 rounded text-cyan-400 hover:text-[#f95ec8] transition"
-                    onClick={onClick}
+                    onClick={handleClick}
                 >
                     <FontAwesomeIcon icon={faQuestion} />
                 </button>
