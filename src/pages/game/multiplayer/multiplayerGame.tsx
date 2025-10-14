17c1071dd20ecea3813a4029683c2948061f3bc0
mariela1982 <marimanri_24@hotmail.com>
2025-10-13 22:01:53 -0300

    menu

--- src/pages/game/multiplayer/multiplayerGame.tsx
+++ src/pages/game/multiplayer/multiplayerGame.tsx
@@ -3,30 +3,28 @@ import { Link } from "react-router-dom";
 
 export const Multiplayer = () => {
 
-   const botongral ="bg-black text-[#FF00FF] border-2 border-[#FF00FF] text-lg py-1 px-6 rounded-[6px] shadow-md transition duration-300 hover:scale-105";
+   const botongral ="bg-black text-[#5DF9F9] border-2 border-[#5DF9F9] text-lg py-1 px-6 rounded-[6px] shadow-md transition duration-300 hover:bg-[#F95EC8] scale-105";
      const botonVolver = "bg-red-600 text-black text-sm  py-2 px-4 rounded-[6px] hover:bg-red-700 transition duration-300";
 
 
    return (
 
          <div
-         className="relative h-screen w-screen  fondo-multijugador"    >
-              <h1 className="text-pink-400 text-5xl  flex justify-center tracking-wide  mb-8 drop-shadow-lg">Multijugador</h1>
-            <div className="absolute inset-0  flex flex-col items-center justify-center space-y-6">
-             
-      
+         className="h-screen w-screen multijugadorContenedor fondo-multijugador"    >
+              <h1 className=" titulo text-pink-400 text-5xl  flex justify-center tracking-wide  mb-8 drop-shadow-lg">Multijugador</h1>
+            <div className="contenedorMenu">
        
-        <div className="grid grid-cols-2 gap-6">
+      
       <Link to="/crear" className={botongral}>Crear Partida</Link>
       <Link to="/unirse-partida" className={botongral}>Unirse a Partida</Link>
       <Link to="/partida-rapida" className={botongral}>Partida Rápida</Link>
       <Link to="/invitar-amigo" className={botongral}>Invitar a un Amigo</Link>
-     </div>
-
-      <Link to="/ranking" className={botongral}>Ranking</Link>
+       <Link to="/ranking" className={botongral}>Ranking</Link>
+ 
+      </div>
+      
       <Link to="/" className={botonVolver}>Volver al Menú</Link>
       
-      </div>
       </div>
 
    
