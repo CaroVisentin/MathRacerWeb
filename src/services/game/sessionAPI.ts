addf6e6e55c0c8c420a50da67dc4e94faf05d01a
Bianca Leites <bleites@circostudio.com>
2025-10-29 18:23:58 -0300

    fix registro

--- src/services/game/sessionAPI.ts
+++ src/services/game/sessionAPI.ts
@@ -4,54 +4,62 @@ import { auth } from "../firebase/firebaseConfig";
 
 export const sessionService = {
     //registro al jugador en firebase
-    registrarUsuario: async(username: string, email: string, password: string) => {
-       
-        try{
-        const userCredential= await createUserWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-        const uid=userCredential.user.uid;
+    registrarUsuario: async (username: string, email: string, password: string) => {
 
-        //crear perfil del jugador en la base de datos
-        const response = await api.post(`${API_URLS.player}`, {
-            username,
-            email,
-            uid,
-        });
-        return response.data;
-    } catch (error:any){
-        throw new Error(error.message) || "No se pudo registrar usuario"    }
+        console.log("Llegó a registrar usuario en sessionService")
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+            const uid = userCredential.user.uid;
+
+            // Crear perfil del jugador en la base de datos
+            const response = await api.post(`${API_URLS.player}`, {
+                username,
+                email,
+                uid,
+            });
+
+            return response.data;
+        } catch (error: any) {
+            throw new Error(error.message) || "No se pudo registrar usuario"
+        }
     },
 
     //login del jugador en firebase
-    loguearUsuario: async (email: string, password: string) =>{
+    loguearUsuario: async (email: string, password: string) => {
         const userCredential = await signInWithEmailAndPassword(
             auth,
             email,
             password
         );
-        const uid= userCredential.user.uid;
+
+        const uid = userCredential.user.uid;
 
         //obtener perfil del jugador desde la api
-         try{
-        const response = await api.get(`${API_URLS.player}/uid/${uid}`);
-        return response.data;
-        }catch (error:any){
-        throw new Error(error.message) || "No se pudo registrar usuario"    }
+        try {
+            const response = await api.get(`${API_URLS.player}/uid/${uid}`);
+
+            return response.data;
+        } catch (error: any) {
+            throw new Error(error.message) || "No se pudo registrar usuario"
+        }
     },
 
-   //obtener perfil del jugador por uid
-    obtenerPerfil:async (uid: string) =>{
-         try{
-         const response = await api.get(`${API_URLS.player}/uid/${uid}`);
-       return response.data;
-        }catch (error:any){
-        throw new Error(error.message) || "No se pudo registrar usuario"  
-      }
+    //obtener perfil del jugador por uid
+    obtenerPerfil: async (uid: string) => {
+        try {
+            const response = await api.get(`${API_URLS.player}/uid/${uid}`);
+            return response.data;
+        } catch (error: any) {
+            throw new Error(error.message) || "No se pudo registrar usuario"
+        }
     },
-    
+
     //cierre de sesion
 
     async logout() {
--- src/services/network/api.ts
+++ src/services/network/api.ts
@@ -1,31 +1,19 @@
 // Configuración del cliente HTTP (axios/fetch)
-
 import axios from "axios";
 
-
 // Detectar entorno
-const baseURL =
-    import.meta.env.MODE === "development"
-        ? import.meta.env.VITE_API_URL_LOCAL
-        : import.meta.env.VITE_API_URL_PROD;
+const baseURL = import.meta.env.VITE_API_URL;
 
 // Exportar instancia base de axios
-// export const api = axios.create({
-//     baseURL: `${baseURL}/api`,
-// });
-
-    export const api = axios.create({
-        baseURL: `${baseURL}/api`,
-        headers:{
-            "Content-Type": "application/json",
-        },
-    })
+export const api = axios.create({
+    baseURL: `${baseURL}/api`,
+});
 
 // Exportar los endpoints organizados por módulo
 export const API_URLS = {
     games: "/games",
-    levels:"/levels",
+    player: "/player",
+    worlds: "/worlds",
+    levels: "/levels",
     online:"/online",
-    player:"/player",
-    worlds:"/worlds"
 };
