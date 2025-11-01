45724f41d25f677503f2a930033e80cdb05c501c
Bianca Leites <bleites@circostudio.com>
2025-11-01 20:56:01 -0300

    error connection modal

--- src/shared/modals/errorConnection.tsx
+++ src/shared/modals/errorConnection.tsx
@@ -2,30 +2,33 @@ import React from "react";
 
 interface ErrorConnectionProps {
   message: string;
-  
   onClose: () => void;
 }
 
-const ErrorConnection: React.FC<ErrorConnectionProps> = ({ message,  onClose }) => {
+const ErrorConnection: React.FC<ErrorConnectionProps> = ({ message, onClose }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-50">
-      <div className="bg-black/90 text-[#5df9f9] p-8 rounded-lg shadow-lg w-full max-w-md text-center font-audiowide">
-        <h2 className="text-3xl text-[#f95ec8] mb-6 drop-shadow-[0_0_10px_#00ffff]">Error de Conexión</h2>
+    <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-[999]">
+      <div className="relative bg-black/90 text-[#5df9f9] p-8 rounded-lg shadow-lg w-full max-w-md text-center">
+        {/* Botón X arriba a la derecha */}
+        <button
+          onClick={onClose}
+          className="absolute top-3 right-3 text-red-500 text-2xl font-bold hover:drop-shadow-[0_0_10px_#f95ec8] transition"
+        >
+          ×
+        </button>
+
+        <h2 className="text-3xl text-[#f95ec8] mb-6 drop-shadow-[0_0_10px_#00ffff]">
+          Error de Conexión
+        </h2>
+
         <p className="text-xl mb-8">{message}</p>
-        <div className="flex justify-center">
-          <button
-            onClick={onClose}
-            className="bg-[#5df9f9] text-black font-extralight hover:bg-red-700 w-32 py-3 rounded text-xl hover:drop-shadow-[0_0_10px_#00ffff]"
-          >
-            Cerrar
-          </button>
-          {/* <button
-            onClick={onRetry}
-            className="bg-[#5df9f9] text-black font-extralight hover:bg-[#f95ec8] w-32 py-3 rounded text-xl hover:drop-shadow-[0_0_10px_#00ffff]"
-          >
-            Reintentar
-          </button> */}
-        </div>
+
+        {/* <button
+          onClick={onRetry}
+          className="!mt-2 bg-[#5df9f9] text-black font-extralight hover:bg-red-700 w-32 py-3 rounded text-xl hover:drop-shadow-[0_0_10px_#00ffff] transition"
+        >
+          Reintentar
+        </button> */}
       </div>
     </div>
   );
