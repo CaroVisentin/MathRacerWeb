a109ba070607ed5894971571c0d8d4f9aa7ba2b6
Bianca Leites <bleites@circostudio.com>
2025-11-08 20:41:32 -0300

    borro context viejo

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import './App.css'
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/cartContext'
 import { EnergyProvider } from './contexts/energyContext';
-// import { PlayerProvider } from './contexts/playerContext';
-// import { StoryModeGameProvider } from './contexts/storyModeGameContext'
 import { AppRouter } from './router/router'
 
 
 function App() {
   return (
     <AuthProvider>
-      {/* <PlayerProvider> */}
         <CartProvider>
           <EnergyProvider>
             <AppRouter />
           </EnergyProvider>
         </CartProvider>
-      {/* </PlayerProvider> */}
     </AuthProvider>
 
   )
--- src/hooks/useStoryModeGame.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import StoryModeGameContext from "../contexts/storyModeGameContext";
-
-export const useStoryModeGame = () => {
-    const context = useContext(StoryModeGameContext)
-    if (!context) throw new Error("useGame must be used within StoryModeGameProvider")
-    return context
-}
