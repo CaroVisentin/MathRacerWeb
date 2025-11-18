ad5f9ae1a40abe8fbd92f20c938f47764c1828c3
Bianca Leites <bleites@circostudio.com>
2025-11-18 10:13:44 -0300

    refactorizo pantalla de reglas

--- src/pages/rules/rules.tsx
+++ src/pages/rules/rules.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import fondoReglas from '../../assets/images/fondo-reglas.png';
-import { rules } from "../../shared/data/rulesData";
+import fondoReglas from "../../assets/images/fondo-reglas.jpg";
 import { useNavigate } from "react-router-dom";
 
 export const RulesPage = () => {
@@ -30,7 +29,7 @@ export const RulesPage = () => {
                     {/* Título dinámico */}
                     <h1
                         className={`flex-1 text-center font-jersey ${step === 1
-                            ? "text-8xl text-[#00f0ff]"
+                            ? "text-7xl text-[#00f0ff]"
                             : "text-5xl text-white"
                             }`}
                     >
@@ -44,14 +43,17 @@ export const RulesPage = () => {
                 <div className="space-y-8 max-w-4xl">
                     {step === 1 && (
                         <>
-                            <p className="text-3xl leading-relaxed">{rules.description}</p>
+                            <p className="text-3xl leading-relaxed">
+                                Math Racer es un juego de carreras donde para avanzar en la pista deberás resolver ecuaciones correctamente.
+                                Cada respuesta acertada hace que tu auto avance y te acerque a la meta.<br />
+                                Si respondes mal, verás la respuesta correcta y recibirás otra ecuación para continuar con el juego.
+                            </p>
 
-                            <h2 className="text-4xl mt-8 mb-4">TIPOS DE DESAFÍO</h2>
+                            <h2 className="text-4xl !mt-8 mb-4">TIPOS DE DESAFÍOS:</h2>
 
                             <ul className="list-disc list-inside text-3xl space-y-4">
-                                {rules.challenge_types.map((challenge) => (
-                                    <li key={challenge.id}>{challenge.title}</li>
-                                ))}
+                                <li> Encuentra el valor de <span className="text-[#00F4FF]">x</span> para que <span className="text-[#00F4FF]">y</span> sea el número mayor posible </li>
+                                <li> Encuentra el valor de <span className="text-[#00F4FF]">x</span> para que <span className="text-[#00F4FF]">y</span> sea el número menor posible </li>
                             </ul>
                         </>
                     )}
@@ -59,16 +61,15 @@ export const RulesPage = () => {
                     {step === 2 && (
                         <>
                             <ul className="list-disc list-inside text-3xl space-y-4">
-                                {rules.game_modes.map((mode) => (
-                                    <li key={mode.title}>
-                                        {mode.title}: {mode.description}
-                                    </li>
-                                ))}
+                                <li> <span className="text-[#00F4FF]">MULTIJUGADOR: </span>Compite contra otros jugadores en carreras rápidas. Gana el que llegue primero a la meta. Cada victoria te da puntos para subir en el ranking</li>
+                                <li> <span className="text-[#00F4FF]">HISTORIA: </span>Juega solo contra la máquina, avanza por niveles y mundos con dificultad creciente. Al superar niveles recibes monedas o premios sorpresa.</li>
                             </ul>
 
-                            <div className="mt-12 space-y-4">
+                            <div className="!mt-12 space-y-4">
                                 <h2 className="text-4xl">VIDAS</h2>
-                                <p className="text-2xl">{rules.lives}</p>
+                                <p className="text-2xl">
+                                    El modo HISTORIA cuenta con 3 vidas, al perder un nivel perderá <span className="text-[#00F4FF]">una vida</span>. Las mismas se recargarán con tiempo o canjeandolas por monedas.
+                                </p>
                             </div>
                         </>
                     )}
@@ -76,7 +77,7 @@ export const RulesPage = () => {
 
                 {/* Botón siguiente/volver */}
                 <button
-                    className="mt-6 bg-[#00f0ff] text-slate-950 border-2 border-white px-8 py-2
+                    className="!mt-6 bg-[#00f0ff] text-slate-950 border-2 border-white px-8 py-2
                     text-xl tracking-wider transition-all duration-300 
                     hover:bg-cyan-400 shadow-[0_0_10px_rgba(0,217,255,0.3)] 
                     hover:shadow-[0_0_20px_rgba(0,217,255,0.6)]"
