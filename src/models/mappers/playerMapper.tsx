3b429fbe96a5c582cf2a7b37b62ef7d5d07691dc
Florencia Carvajal <florcar1998@gmail.com>
2025-11-26 15:08:06 -0300

    correccion

--- src/models/mappers/playerMapper.tsx
+++ src/models/mappers/playerMapper.tsx
@@ -1,18 +1,18 @@
 import type { BackendPlayer, Player } from "../ui/player/player";
 
 export function mapToUiPlayer(data: BackendPlayer): Player {
-    return {
-        id: data?.id ?? 0,
-        name: data?.name ?? "",
-        email: data?.email ?? "",
-        lastlevelId: data?.lastlevelId ?? 0,
-        points: data?.points ?? 0,
-        coins: data?.coins ?? 0,
-        background: data?.background ?? data?.equippedBackground ?? null,
-        car: data?.car ?? data?.equippedCar ?? null,
-        character: data?.character ?? data?.equippedCharacter ?? null,
-        equippedBackground: data?.equippedBackground ?? data?.background ?? null,
-        equippedCar: data?.equippedCar ?? data?.car ?? null,
-        equippedCharacter: data?.equippedCharacter ?? data?.character ?? null,
-    };
+  return {
+    id: data?.id ?? 0,
+    name: data?.name ?? "",
+    email: data?.email ?? "",
+    lastLevelId: data?.lastLevelId ?? 0,
+    points: data?.points ?? 0,
+    coins: data?.coins ?? 0,
+    background: data?.background ?? data?.equippedBackground ?? null,
+    car: data?.car ?? data?.equippedCar ?? null,
+    character: data?.character ?? data?.equippedCharacter ?? null,
+    equippedBackground: data?.equippedBackground ?? data?.background ?? null,
+    equippedCar: data?.equippedCar ?? data?.car ?? null,
+    equippedCharacter: data?.equippedCharacter ?? data?.character ?? null,
+  };
 }
